56ce599947e9a76b80ef024eb9dd168415afc5fa
tilin <adririsco05@gmail.com>
2026-02-01 12:04:13 +0100

    Update permisosUsuarios.sql

--- sql/permisosUsuarios.sql
+++ sql/permisosUsuarios.sql
@@ -2,23 +2,17 @@
 
 -- CREACIÓN DE USUARIOS Y PERMISOS
 
--- Usuario 'admin': Este usuario será el administrador de la base de datos y podrá realizar cualquier operación (INSERT, UPDATE, DELETE, CREATE, DROP, ALTER…).
+-- Usuario 'admin': Este usuario será el administrador de la base de datos.
 CREATE USER 'admin'@'localhost' IDENTIFIED BY 'admin123'; 
 
--- Usuario 'editor': Este usuario puede leer y modificar datos (SELECT, UPDATE) tiene más permisos pero no puede crear, modificar o borrar estructura de bbdd por seguridad.
-CREATE USER 'editor'@'localhost' IDENTIFIED BY 'editor123'; 
-
--- Usuario 'usuario': Este usuario solo tendrá permisos de lectura (SELECT). Este usuario no puede modificar la base de datos lo cuál lo hace seguro si no quieres que modifique filas o estructura de la tabla.
+-- Usuario 'usuario': Este usuario será el usuario de cualquiera que entre a la web.
 CREATE USER 'usuario'@'localhost' IDENTIFIED BY 'usuario123'; 
 
 
 -- ASIGNACIÓN DE PERMISOS
 
--- Usuario admin: administra la base de datos, puede crear, modificar y eliminar tablas y datos.
+-- Usuario 'admin': Administra la base de datos, puede crear, modificar y eliminar tablas y datos.
 GRANT ALL PRIVILEGES ON deportes.* TO 'admin'@'localhost';
 
--- Usuario editor: puede ver, modificar y borrar datos existentes sin alterar la estructura de la base de datos.
-GRANT SELECT, UPDATE, DELETE ON deportes.* TO 'editor'@'localhost';
-
--- Usuario usuario: El usuario de cualquiera que entre a la web tiene los permisos suficientes para ver la web y para inscribirse.
+-- Usuario 'usuario': El usuario de cualquiera que entre a la web tiene los permisos suficientes para ver la web y para inscribirse.
 GRANT SELECT, UPDATE ON deportes.* TO 'usuario'@'localhost';
